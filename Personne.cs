fdfc73544807d59b16ffaa4e9824b81b56cd2d22
JoatypeR <weryjoachim@gmail.com>
2024-02-24 17:09:21 +0100

    Changement avant reprise

--- Personne.cs
+++ Personne.cs
@@ -9,8 +9,13 @@
         {
             try
             {
-                var data = value.Split(' ');
+                var data = value.Split(' '); //split renvoie un tebleau de chaine de caractère
                 person = new Personne();
+                {
+                    Nom = data[0]; //Le trableau renvoie le nom et le prénom
+                    Prenom = data[1]; 
+                }
+                //Si ça se passe sans problème, il renvoie true
                 return true;
 
             }
