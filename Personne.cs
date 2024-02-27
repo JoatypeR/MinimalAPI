4ad3b56efc9e7a65b35794b6bcc6183d6333140b
JoatypeR <weryjoachim@gmail.com>
2024-02-27 11:43:24 +0100

    Détermination du tableau Asynchrone

--- Personne.cs
+++ Personne.cs
@@ -1,7 +1,12 @@
-﻿namespace MinimalAPI
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using System.Reflection;
+
+namespace MinimalAPI
 {
     public class Personne
     {
+        private object person;
+
         public string Nom { get; set; }
         public string Surname { get; set; }
 
@@ -24,28 +29,28 @@
         //        person = null;
         //        return true;
         //    }
-        public async Task<Personne> BindAsync(
+        public static async ValueTask<Personne> BindAsync(
             HttpContext context, ParameterInfo parameterInfo)
         {
             try
             {
-                var data = value.Split(' '); //split renvoie un tebleau de chaine de caractère
-                person = new Personne
+                using var StreamReader = new StreamReader(context.Request.Body);
+                var body = await StreamReader.ReadToEndAsync();
+                var data = body.Split(' ');
+                var person = new Personne
                 {
-                    Nom = data[0], //Le trableau renvoie le nom et le prénom
+                    Nom = data[0], //Le tableau renvoie le nom et le prénom
                     Surname = data[1]
                 };
                 //Si ça se passe sans problème, il renvoie true
-                return true;
+                return person;
 
             }
             catch (Exception)
             {
-                person = null;
-                return true;
+                return null;
             }
 
-        }
         }
     }
 }
